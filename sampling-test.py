8229f6e8ecd09f6f1af223ff7cdae78ba1bd9525
Ryan <quezzertx@gmail.com>
2025-08-20 20:15:38 +1000

    Basic mapping function with random input variables

--- sampling-test.py
+++ sampling-test.py
@@ -1,3 +1,19 @@
-import math
+import numpy
+import matplotlib.pyplot as plt
 
-print(1+2)
+n = 100000
+
+x_0 = numpy.random.normal(loc = 36, scale = 0.4, size = n)
+v = numpy.random.normal(loc = 5, scale = 0.2, size = n)
+
+def mapping(a, b):
+    result = []
+
+    if len(a) == len(b):
+        for i in range(len(a)):
+            result.append(a[i] + 2 * b[i])
+    
+    return result
+
+plt.hist(mapping(x_0,v))
+plt.show()
